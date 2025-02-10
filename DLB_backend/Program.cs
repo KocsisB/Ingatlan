427bcfb9184742eb2ac7c6f1c7a8071428239d94
László Tamás <laszlot@kkszki.hu>
2025-02-10 14:21:08 +0100

    cors policy AGAIN

--- DLB_backend/Program.cs
+++ DLB_backend/Program.cs
@@ -5,6 +5,19 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<DlbLakaskulcsContext>();
 // Add services to the container.
 
+// Enged�lyezz�k a CORS-t
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("CorsPolicy",
+        policy =>
+        {
+            policy.WithOrigins("http://localhost:3000")
+                   .AllowAnyMethod()
+                   .AllowAnyHeader();
+        });
+});
+
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -19,6 +32,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors("CorsPolicy"); // Alkalmazzuk a CORS szab�lyzatot
 
 app.UseHttpsRedirection();
 
