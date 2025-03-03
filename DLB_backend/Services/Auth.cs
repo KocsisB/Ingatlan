e78160305a22adeb53e1ae63427abc54f4365177
KocsisB <kocsisb@kkszki.hu>
2025-03-03 13:21:29 +0100

    Auth.cs is refreshed

--- DLB_backend/Services/Auth.cs
+++ DLB_backend/Services/Auth.cs
@@ -3,6 +3,8 @@ using DLB_backend.Models;
 using DLB_backend.Services.Dtos;
 using DLB_backend.Services.IAuthService;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace DLB_backend.Services
 {
@@ -17,19 +19,33 @@ namespace DLB_backend.Services
             this.userManager = userManager;
         }
 
-        public Task<object> AssignRole(string email, string roleName)
+        public async Task<object> AssignRole(string email, string roleName)
         {
             throw new NotImplementedException();
         }
 
-        public Task<object> Login(LoginIUserDto loginIUserDto)
+        public async Task<object> Login(LoginIUserDto loginIUserDto)
         {
             throw new NotImplementedException();
         }
 
-        public Task<object> Register(CreateUserDto createUserDto)
+        public async Task<object> Register(CreateUserDto createUserDto)
         {
-            throw new NotImplementedException();
+            var user = new Felhasznalok
+            {
+                UserName = createUserDto.UserName,
+                Email = createUserDto.Email
+            };
+
+            var result = await userManager.CreateAsync(user, createUserDto.Password);
+            if (result.Succeeded)
+            {
+                var userReturn = await _dbContext.applicationUsers.FirstOrDefaultAsync(user=>user.UserName == createUserDto.UserName);
+
+                return new { result = userReturn, message = "Sikeres regisztráció." };
+            }
+
+            return new { result = "", message = result.Errors.FirstOrDefault().Description };
         }
     }
 }
