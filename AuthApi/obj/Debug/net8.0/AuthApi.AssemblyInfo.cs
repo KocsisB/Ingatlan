3def304d8c36abed077ac5ab967c64bced7ee933
KocsisB <kocsisb@kkszki.hu>
2025-03-04 13:03:21 +0100

    Test

--- AuthApi/obj/Debug/net8.0/AuthApi.AssemblyInfo.cs
+++ AuthApi/obj/Debug/net8.0/AuthApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuthApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+67a7c475f7047b00938ebc331ad19093030643ef")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuthApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuthApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
