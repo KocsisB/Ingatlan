5f2ccd0a510f165c7c2619b2f31912d95e28a25c
KocsisB <90753544+KocsisB@users.noreply.github.com>
2025-03-11 12:05:41 +0100

    Delete previous version

--- DLB_backend/Models/JwtOptions.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace DLB_backend.Models
-{
-    public class JwtOptions
-    {
-        public string Issuer { get; set; }
-        public string Audience { get; set; }
-        public string Secret { get; set; }
-    }
-}
--- DLB_backend/Services/Dtos/Dto.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace DLB_backend.Services.Dtos
-{
-    public record CreateUserDto(string UserName, string Email, string Password, DateTime BirthDate, string PhoneNumber);
-
-    public record LoginIUserDto(string UserName, string Password);
-
-    public record AssignUserDto(string Email, string RoleName);
-}
