5b413fcfa18ffa07d73ede24946100ae868959d9
norbertstu <91181165+Lacika16@users.noreply.github.com>
2025-03-28 11:32:10 +0100

    hazmodositas.js updated

--- src/HazModositas.js
+++ src/HazModositas.js
@@ -1,96 +1,170 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { useParams } from 'react-router-dom';
-import './ProfilModositas.css';
+import { useNavigate, useParams } from 'react-router-dom';
+import './Hazmodositas.css';
 
-export default function ProfilModositas() {
-  const { id } = useParams();
-  const [userProfile, setUserProfile] = useState({
-    name: '',
-    email: '',
-    password: ''
+export default function Hazmodositas() {
+  const [property, setProperty ] = useState({
+    cim: '',
+    varos: '',
+    allapot: '',
+    telekMerete: '',
+    epitesVege: '',
+    ar: '',
+    alapterulet: '',
+    szobakSzama: '',
+    kep: ''
   });
-  const [error, setError] = useState(null);
+  const navigate = useNavigate();
+  const { id } = useParams();
 
   useEffect(() => {
-    const fetchUserData = async () => {
+    // Fetch property data when component mounts
+    const fetchProperty = async () => {
       try {
-        const response = await axios.get(`${process.env.REACT_APP_API_URL}/profile/${id}`);
-        setUserProfile(response.data);
+        const response = await axios.get(`${process.env.REACT_APP_API_URL}/ingatlan/${id}`);
+        setProperty(response.data);
       } catch (error) {
-        setError('Hiba történt az adatok lekérése során: ' + error.message);
-        console.error('Error fetching user data:', error);
+        console.error("Error fetching property data:", error);
       }
     };
 
-    fetchUserData();
+    fetchProperty();
   }, [id]);
 
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setUserProfile((prevProfile) => ({
-      ...prevProfile,
-      [name]: value
-    }));
-  };
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
+  const handleUpdate = async () => {
     try {
-      const response = await axios.put(`${process.env.REACT_APP_API_URL}/profile/${id}`, userProfile);
-      console.log(response.data);
-      alert('Profile updated successfully!');
+      const updatedProperty = {
+        ...property,
+      };
+
+      const response = await axios.put(`${process.env.REACT_APP_API_URL}/ingatlan/${id}`, updatedProperty);
+      console.log(response);
+      alert("Sikeres frissítés!");
+      navigate('/eladohazak');
     } catch (error) {
-      console.error('Error updating profile:', error);
-      alert('Failed to update profile.');
+      console.error("Error updating property:", error);
+      alert("Sikertelen frissítés!");
     }
   };
 
-  if (error) {
-    return <div className="profilmodositas-container"><p>{error}</p></div>;
-  }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    handleUpdate();
+  };
+
+  const handleChange = (e) => {
+    const { id, value, files, type } = e.target;
+    setProperty((prevProperty) => ({
+      ...prevProperty,
+      [id]: type =="file" ? files[0]: value,
+    }));
+  };
 
   return (
-    <div className="profilmodositas-container">
-      <h1 className="profilmodositas-title">Profil Módosítása</h1>
-      <form onSubmit={handleSubmit} className="profilmodositas-form">
-        <div className="profilmodositas-form-group">
-          <label htmlFor="name" className="profilmodositas-label">Név:</label>
+    <div className="container-hirdetes">
+      <h1 className="form-title">Ház módosítása</h1>
+      <form onSubmit={handleSubmit} className="form-container">
+        <div className="form-group">
+          <label htmlFor="cim" className="form-label">Cím</label>
           <input
             type="text"
-            id="name"
-            name="name"
-            value={userProfile.name}
+            className="formControl"
+            id="cim"
+            value={property.cim}
             onChange={handleChange}
-            className="profilmodositas-input"
             required
           />
         </div>
-        <div className="profilmodositas-form-group">
-          <label htmlFor="email" className="profilmodositas-label">Email:</label>
+        <div className="form-group">
+          <label htmlFor="varos" className="form-label">Város</label>
           <input
-            type="email"
-            id="email"
-            name="email"
-            value={userProfile.email}
+            type="text"
+            className="formControl"
+            id="varos"
+            value={property.varos}
             onChange={handleChange}
-            className="profilmodositas-input"
             required
           />
         </div>
-        <div className="profilmodositas-form-group">
-          <label htmlFor="password" className="profilmodositas-label">Jelszó:</label>
+        <div className="form-group">
+          <label htmlFor="allapot" className="form-label">Állapot</label>
           <input
-            type="password"
-            id="password"
-            name="password"
-            value={userProfile.password}
+            type="text"
+            className="formControl"
+            id="allapot"
+            value={property.allapot}
             onChange={handleChange}
-            className="profilmodositas-input"
             required
           />
         </div>
-        <button type="submit" className="profilmodositas-button">Módosítás</button>
+        <div className="form-group">
+          <label htmlFor="telekMerete" className="form-label">Telek Mérete</label>
+          <input
+            type="number"
+            className="formControl"
+            id="telekMerete"
+            value={property.telekMerete}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="epitesVege" className="form-label">Építés vége</label>
+          <input
+            type="number"
+            className="formControl"
+            id="epitesVege"
+            value={property.epitesVege}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="ar" className="form-label">Ár</label>
+          <input
+            type="number"
+            className="formControl"
+            id="ar"
+            value={property.ar}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="alapterulet" className="form-label">Alapterület</label>
+          <input
+            type="number"
+            className="formControl"
+            id="alapterulet"
+            value={property.alapterulet}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="szobakSzama" className="form-label">Szobák száma</label>
+          <input
+            type="number"
+            className="formControl"
+            id="szobakSzama"
+            value={property.szobakSzama}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="kepUrl" className="form-label">Kép feltöltése</label>
+          <input
+            type="file"
+            className="formControl"
+            id="kep"
+            value={property.kep}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <button type="submit" className="submit-button">Ház frissítése</button>
       </form>
     </div>
   );
